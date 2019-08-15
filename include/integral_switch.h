1a9dfd2bc8d58914e5728a2e45026b82ac7405ab
qian <yuqian1990@gmail.com>
2019-08-15 23:47:43 +0800

    Added simplest implementation with motivation example

--- include/integral_switch.h
+++ include/integral_switch.h
@@ -1,5 +1,32 @@
+#ifndef INTEGRAL_SWITCH_H_
+#define INTEGRAL_SWITCH_H_
+
+#include <type_traits>
+
 namespace integral_switch{
 
-class integral_switch;
+template<typename T, T... v>
+struct integral_switch
+{
+    template<typename Visitor>
+    using return_type = std::common_type<decltype(std::declval<Visitor>()(std::integral_constant<T, v>{}))...>;
 
+    template<typename Visitor>
+    static typename return_type<Visitor>::type visit(Visitor&& visitor, T value)
+    {
+        switch(value)
+        {
+            case 0:
+                return visitor(std::integral_constant<T, 0>{});
+            case 1:
+                return visitor(std::integral_constant<T, 1>{});
+            case 2:
+                return visitor(std::integral_constant<T, 2>{});
+        }
+
+        throw std::invalid_argument("value");
+    }
+};
 }
+
+#endif
--- test/simple_test.cpp
+++ test/simple_test.cpp
@@ -1,11 +1,35 @@
+#include <type_traits>
 #include <gtest/gtest.h>
 
 #include "integral_switch.h"
 
 namespace integral_switch{
 
+struct FooVisitor
+{
+    constexpr int operator()(std::integral_constant<int, 0>) const
+    {
+        return 10;
+    }
+
+    constexpr int operator()(std::integral_constant<int, 1>) const
+    {
+        return 20;
+    }
+
+    constexpr int operator()(std::integral_constant<int, 2>) const
+    {
+        return 30;
+    }
+};
+
 TEST(simple_test, test0) // NOLINT
 {
-    ASSERT_EQ(0, 0);
+    using foo_switch = integral_switch<int, 0, 1, 2>;
+
+    FooVisitor visitor;
+    ASSERT_EQ(10, foo_switch::visit(visitor, 0));
+    ASSERT_EQ(20, foo_switch::visit(visitor, 1));
+    ASSERT_EQ(30, foo_switch::visit(visitor, 2));
 }
 }
