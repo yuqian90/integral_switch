93f49e735fe82caf8b325f093255c604b117f5f1
qian <yuqian1990@gmail.com>
2019-08-18 23:02:26 +0800

    Add call operator test for c++11

--- test/simple_test.cpp
+++ test/simple_test.cpp
@@ -20,7 +20,9 @@ constexpr int visit(std::integral_constant<int, 2>)
     return 30;
 }
 
-TEST(simple_test, test0) // NOLINT
+#if __cpp_generic_lambdas
+
+TEST(simple_test, genericlambda) // NOLINT
 {
     using foo_switch = integral_switch<int, 0, 1, 2>;
     // generic lambda is c++14 only.
@@ -31,4 +33,34 @@ TEST(simple_test, test0) // NOLINT
     ASSERT_EQ(20, foo_switch::visit(visitor, 1));
     ASSERT_EQ(30, foo_switch::visit(visitor, 2));
 }
+
+#endif
+
+struct Visitor
+{
+    constexpr int operator()(std::integral_constant<int, 0>) const
+    {
+        return 10;
+    }
+
+    constexpr int operator()(std::integral_constant<int, 1>) const
+    {
+        return 20;
+    }
+
+    constexpr int operator()(std::integral_constant<int, 2>) const
+    {
+        return 30;
+    }
+};
+
+TEST(simple_test, calloperator) // NOLINT
+{
+    using foo_switch = integral_switch<int, 0, 1, 2>;
+    Visitor visitor;
+    ASSERT_EQ(10, foo_switch::visit(visitor, 0));
+    ASSERT_EQ(20, foo_switch::visit(visitor, 1));
+    ASSERT_EQ(30, foo_switch::visit(visitor, 2));
+}
+
 }
